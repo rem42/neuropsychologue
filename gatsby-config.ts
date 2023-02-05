ac3c94537f04206a3412fb405000c578666d921d
rem42 <me@remy.ovh>
2023-02-05 21:13:16 +0100

    feat: add sitemap

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,13 +5,14 @@ const config: GatsbyConfig = {
     title: `Neuropsychologue enfants et adultes à Lyon, Brignais | HÉBERT NEUROPSYCHOLOGUE`,
     // description: `Example project for the Gatsby Head API`,
     image: `/src/images/favicon.svg`,
-    siteUrl: `https://hebertneuropsychologue.com`,
+    siteUrl: `https://hebertneuropsychologue.fr`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
